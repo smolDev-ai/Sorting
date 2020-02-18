813126cd40fa0b720faea618727c68452037def6
Megan <megan.miller@smoldev.dev>
2020-02-18 15:37:15 -0600

    completed merge_sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,67 +1,69 @@
-# [5 9 3 7 2 8 1 6]
-​
-# [3 2 1][5][9 7 8 6]
-​
-# [2 1][3][][5][7 8 6][9][]
-​
-# [1][2][3][5] [6][7][8] [9] 
-​
-​
-# Decide on base case
-# base case is []
-​
-# Find the pivot point
-​
-# partition our data to the left and right of the pivot
-# left -> smaller than pivot, right -> larger than pivot
-# What if they're the same size as the pivot? Just pick one?  >=
-​
-​
-# repeat, recurse
-​
-my_list = [5, 9, 3, 7, 2, 8, 1, 6]
-​
-​
-def partition(data):
-    left = []
-    pivot = data[0]
-    right = []
-​
-    for item in data[1:]:
-        if item < pivot:
-            left.append(item)
-        else: # Handling > or =
-            right.append(item)
-​
-    return left, pivot, right
-​
-def quicksort(data):
-    if data == []:
-        return data
+# def partition(data):
+#     left = []
+#     pivot = data[0]
+#     right = []
+
+#     for item in data[1:]:
+#         if item < pivot:
+#             left.append(item)
+#         else: # Handling > or =
+#             right.append(item)
+# ​
+#     return left, pivot, right
+# ​
+# def quicksort(data):
+#     if data == []:
+#         return data
     
-    left, pivot, right = partition(data)
-​
-    return quicksort(left) + [pivot] + quicksort(right)
-​
-print(quicksort(my_list))
+#     left, pivot, right = partition(data)
+# ​
+#     return quicksort(left) + [pivot] + quicksort(right)
+# ​
+# print(quicksort(my_list))
 
 
 
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    a = 0
+    b = 0
+    m = 0
+
+    while a < len(arrA) and b < len(arrB):
+        if arrA[a] > arrB[b]:
+            merged_arr[m] = arrB[b]
+            b += 1
+        elif arrA[a] < arrB[b]:
+            merged_arr[m] = arrA[a]
+            a += 1
+        m += 1
+
+    while a < len(arrA):
+        merged_arr[m] = arrA[a]
+        a += 1
+        m += 1
+
+    while b < len(arrB):
+        merged_arr[m] = arrB[b]
+        b += 1
+        m += 1
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
-   
-
-    return arr
+    if arr == [] or len(arr) < 2:
+        return arr
+    
+    left = merge_sort(arr[len(arr) // 2:])
+    right = merge_sort(arr[:len(arr) // 2])
+    
+    return merge(left, right)
 
 
 # STRETCH: implement an in-place merge sort algorithm
